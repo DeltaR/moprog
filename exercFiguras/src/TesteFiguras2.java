493cd00b4a0a7c9b4fc28e0d1a034ac8dbd92de3
Deltini <1121482@isep.ipp.pt>
2016-04-22 15:30:20 +0100

    first commit

---
+++ exercFiguras/src/TesteFiguras2.java
@@ -0,0 +1,47 @@
+public class TesteFiguras2 {
+
+    public static void main(String[] args){
+        System.out.println("\n### Figuras Construídas ###");
+        Circulo c1 = new Circulo();
+        System.out.println(c1);
+
+        Circulo c2 = new Circulo(5, "amarelo");
+        System.out.println(c2);
+
+        Retangulo r1 = new Retangulo();
+        System.out.println(r1);
+
+        Retangulo r2 = new Retangulo(5, 2, "azul");
+        System.out.println(r2);
+
+        System.out.println("\n### Variáveis do Tipo Figuras ###");
+        Figura obj;
+        obj = c1;
+        System.out.println(obj.toString());
+
+        obj = r1;
+        System.out.println(obj.toString());
+
+        // Armazenamento de objetos num contentor do tipo Array
+        Figura[] figuras = new Figura[10];
+
+        figuras[0] = c1;
+        figuras[1] = c2;
+        figuras[2] = r1;
+        figuras[3] = r2;
+
+        System.out.println("\n### Listagem de Figuras Armazenadas no Contentor ###");
+        for (int i = 0; i < figuras.length; i++) {
+            if (figuras[i] != null) {
+                System.out.println(figuras[i]);
+            }
+        }
+
+        System.out.println("\n### Listagem das Áreas das Figuras ###");
+        for (int i = 0; i < figuras.length; i++) {
+            if (figuras[i] != null) {
+                System.out.printf("%s área=%.1f%n", figuras[i], (figuras[i].calcularArea()));
+            }
+        }
+    }
+}
